ef92a2c74a4be85935c4d7089561c9ebe6dd9217
PraditaAdiantama <aditadiantama@gmail.com>
2023-12-01 19:00:45 +0700

    feat: add presensi menu component

---
+++ resources/views/components/presensi/menu.blade.php
@@ -0,0 +1,46 @@
+<div class="menu">
+    <h3>Jurusan</h3>
+    <div class="major">
+        <button>TJKT</button>
+        <button>TM</button>
+        <button>TKR</button>
+        <button>TITL</button>
+        <button>LAS</button>
+        <button>TOI</button>
+        <button>DKV</button>
+        <button>KA</button>
+        <button>BCPF</button>
+    </div>
+</div>
+<!-- major menu -->
+<div class="menu">
+    <h3>Kelas</h3>
+    <div class="class">
+        <label>
+            <input type="radio" name="class">
+            X
+        </label>
+        <label>
+            <input type="radio" name="class">
+            XI
+        </label>
+        <label>
+            <input type="radio" name="class">
+            XII
+        </label>
+        <select name="" id="">
+            <option value="">TJKT 1</option>
+            <option value="">TJKT 2</option>
+            <option value="">TJKT 3</option>
+            <option value="">TJKT 4</option>
+        </select>
+    </div>
+</div>
+<div class="menu">
+    <h3>Mapel</h3>
+    <div class="subject">
+        <button>Produktif</button>
+        <button>Normada</button>
+        <select name="" id=""></select>
+    </div>
+</div>
