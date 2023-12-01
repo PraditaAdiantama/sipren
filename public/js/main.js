21db0b223ee585c8526ddb79ef378ff8828cfa38
PraditaAdiantama <aditadiantama@gmail.com>
2023-12-01 16:00:21 +0700

    feat: add dynamic timestamp for dashbord

--- public/js/main.js
+++ public/js/main.js
@@ -3,10 +3,21 @@ const showSideBtn = document.querySelector('button.menu-btn');
 const statusContent = document.querySelector('.footer .status .content');
 const sidebar = document.querySelector('.sidebar');
 
+const dashboardWelcome = document.querySelector('.main .welcome-selayang');
+
 if (statusContent) {
     statusHideBtn.addEventListener('click', () => {
         statusContent.classList.toggle('active');
         statusHideBtn.classList.toggle('active');
         statusIsHide = true
     });
+}
+
+if (dashboardWelcome) {
+    document.querySelector('.main .history-date').style.height = dashboardWelcome.clientHeight + 'px';
+    setInterval(() => {
+        const date = new Date();
+        document.getElementById('alert_timestamp').innerHTML = `${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`;
+        document.getElementById('date_time').innerHTML = `${date.getDate()}-${date.getMonth()}-${date.getFullYear()}`;
+    }, 1000);
 }
