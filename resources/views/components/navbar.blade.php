0a6e1c43bc13b58840b231e4ddc1e991330ebaa6
PraditaAdiantama <aditadiantama@gmail.com>
2023-12-01 15:59:40 +0700

    feat: add navbar component

---
+++ resources/views/components/navbar.blade.php
@@ -0,0 +1,22 @@
+<header class="header">
+    <div class="container">
+        <h3>BERANDA</h3>
+        <div>
+            <div class="user">
+                <p>Nama Guru</p>
+                <span>Guru TJKT</span>
+            </div>
+            <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" viewBox="0 0 63 64" fill="none">
+                <path
+                    d="M17.25 48.9125V46.0625C17.25 38.1925 23.63 31.8125 31.5 31.8125C39.37 31.8125 45.75 38.1925 45.75 46.0625V48.9125"
+                    stroke="black" stroke-width="6" stroke-linecap="round" />
+                <path
+                    d="M31.5 31.8125C36.2221 31.8125 40.05 27.9847 40.05 23.2625C40.05 18.5405 36.2221 14.7125 31.5 14.7125C26.7778 14.7125 22.95 18.5405 22.95 23.2625C22.95 27.9847 26.7778 31.8125 31.5 31.8125Z"
+                    stroke="black" stroke-width="6" stroke-linecap="round" stroke-linejoin="round" />
+                <path
+                    d="M31.5 60.3125C47.2401 60.3125 60 47.5526 60 31.8125C60 16.0724 47.2401 3.3125 31.5 3.3125C15.7599 3.3125 3 16.0724 3 31.8125C3 47.5526 15.7599 60.3125 31.5 60.3125Z"
+                    stroke="black" stroke-width="6" />
+            </svg>
+        </div>
+    </div>
+</header>
